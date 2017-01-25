32d99794a52cbc7deda2b132bbb242898b8dfe51
Geetha <geethasrinivas30@gmail.com>
2017-01-25 12:32:35 -0500

    initial commit

---
+++ MavenDemo1/src/test/java/com/testing/MavenDemo1/GoogleTest.java
@@ -0,0 +1,18 @@
+package com.testing.MavenDemo1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+
+@Test
+public void testGoogle()
+{
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://google.com");
+	
+}
+
+}
---
+++ MavenDemo1/src/test/java/com/testing/MavenDemo1/LoginErrorTest.java
@@ -0,0 +1,21 @@
+package com.testing.MavenDemo1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginErrorTest {
+	@Test
+	public void testLoginError()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.manage().window().maximize();
+		 driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("anca");
+		 driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("aaa");
+		 driver.findElement(By.xpath(".//*[@id='Login']")).click();
+		 String str = driver.findElement(By.xpath(".//*[@id='error']")).getText();
+		 System.out.println(str);
+	}
+}
